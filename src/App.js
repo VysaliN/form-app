9c5d4b740bf5ecc9ec3442f27dc365f04e42c9e8
vysali <vyshuammu2000@gmail.com>
2022-10-18 00:03:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import React from 'react'
+// import Validation from "./Validation";
+// 
+// const App = () => {
+//   return (
+	// <div><Validation /></div>
+//   )
+// }
+// 
+// export default App
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+import Form from './Form';
+// 
+function App() {
+  return (
+    <div className="App">
+        <Form/>
+    </div>
+  );
+}
+// 
+// 
+export default App;
